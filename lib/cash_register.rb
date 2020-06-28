07d65ff4e68bea8e01059502721ec8fd0161d2d3
Tomas Perez <tomasitopg6@gmail.com>
2020-06-28 01:57:24 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -35,7 +35,7 @@ class CashRegister
 
 
   def void_last_transaction
-    @last_transaction_amount = @total
+    @last_transaction_amount = @total * @items
     @total -= @last_transaction_amount
   end
 end

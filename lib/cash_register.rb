7ee8c6eebb6cb7cc34ab92a0ec9dc34569b94f1d
Tomas Perez <tomasitopg6@gmail.com>
2020-06-28 02:05:34 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,6 +1,6 @@
 class CashRegister
   
-  attr_accessor :total, :discount, :last_transaction_amount, :items, :price
+  attr_accessor :total, :discount, :last_transaction_amount, :items
   
   def initialize(discount = 0)
     @total = 0

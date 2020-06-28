2465a6dce4fa6193308e40a9b517c12bd4b9db12
Tomas Perez <tomasitopg6@gmail.com>
2020-06-28 01:52:31 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -23,7 +23,20 @@ class CashRegister
     @total
   end
   
-  
+  def apply_discount()
+    if @discount > 0
+      @discount = @discount/100.to_f
+      @total = @total - (@total * (@discount))
+      "After the discount, the total comes to $#{@total.to_i}."
+    else
+      "There is no discount to apply."
+    end
+  end
+
+
+  def void_last_transaction()
+    @total -= @last_transaction_amount
+  end
   
   
   

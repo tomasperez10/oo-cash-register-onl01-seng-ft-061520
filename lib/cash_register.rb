5989d64927cab02e159c54b2c736bdfcdee553a1
Tomas Perez <tomasitopg6@gmail.com>
2020-06-28 01:51:42 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -8,7 +8,7 @@ class CashRegister
     @items = []
   end
   
-  def add_item(title,price,quantity = 1)
+  def add_item(title, price, quantity = 1)
     if quantity > 1
       i=0
       while i<quantity
